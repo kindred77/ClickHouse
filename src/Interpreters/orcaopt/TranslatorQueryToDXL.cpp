73d26e0d4f0e2f2a87ff58f08b0956c7daac8f85
kindred77 <109327586@qq.com>
2022-01-12 09:16:15 +0800

    initialize

---
+++ src/Interpreters/orcaopt/TranslatorQueryToDXL.cpp
@@ -0,0 +1,18 @@
+/*
+ * TranslatorQueryToDXL.cpp
+ *
+ *  Created on: Jan 11, 2022
+ *      Author: kindred
+ */
+
+#include "TranslatorQueryToDXL.h"
+
+TranslatorQueryToDXL::TranslatorQueryToDXL() {
+    // TODO Auto-generated constructor stub
+
+}
+
+TranslatorQueryToDXL::~TranslatorQueryToDXL() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ src/Interpreters/orcaopt/TranslatorQueryToDXL.h
@@ -0,0 +1,17 @@
+/*
+ * TranslatorQueryToDXL.h
+ *
+ *  Created on: Jan 11, 2022
+ *      Author: kindred
+ */
+
+#ifndef SRC_INTERPRETERS_ORCAOPT_TRANSLATORQUERYTODXL_H_
+#define SRC_INTERPRETERS_ORCAOPT_TRANSLATORQUERYTODXL_H_
+
+class TranslatorQueryToDXL {
+public:
+    TranslatorQueryToDXL();
+    virtual ~TranslatorQueryToDXL();
+};
+
+#endif /* SRC_INTERPRETERS_ORCAOPT_TRANSLATORQUERYTODXL_H_ */
